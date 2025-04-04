1078aa5f2b3049325881e381781b4a23d72f77d8
rmckeeBuf <rtmckee@buffalo.edu>
2025-04-04 17:41:21 -0400

    Add files via upload

---
+++ MPJava2 (3)/src/ub/cse/algo/Solution.java
@@ -0,0 +1,35 @@
+package ub.cse.algo;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    private Info info;
+    private Graph graph;
+    private ArrayList<Client> clients;
+    private ArrayList<Integer> bandwidths;
+
+    /**
+     * Basic Constructor
+     *
+     * @param info: data parsed from input file
+     */
+    public Solution(Info info) {
+        this.info = info;
+        this.graph = info.graph;
+        this.clients = info.clients;
+        this.bandwidths = info.bandwidths;
+    }
+
+    /**
+     * Method that returns the calculated 
+     * SolutionObject as found by your algorithm
+     *
+     * @return SolutionObject containing the paths, priorities and bandwidths
+     */
+    public SolutionObject outputPaths() {
+        SolutionObject sol = new SolutionObject();
+        /* TODO: Your solution goes here */
+        return sol;
+    }
+}
